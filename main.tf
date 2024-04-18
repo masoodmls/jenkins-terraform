4505b23785f54d7f8a4da4bbf81d4e1e50bb4d4c
Masood Ahmed <masoodmls@gmail.com>
2024-04-19 04:46:05 +0530

    last

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ resource "aws_instance" "rpsweb" {
   ami                    = "ami-0a1b648e2cd533174"
   instance_type          = "t2.micro"
   tags = {
-    Name = "rps-web"
+    Name = "rps-web1"
   }
 
